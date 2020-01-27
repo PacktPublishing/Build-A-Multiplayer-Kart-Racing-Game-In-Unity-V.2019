9031f5a3a106e8239573e21344733e304d851aca
praisyj <59476260+praisyj@users.noreply.github.com>
2020-01-27 14:49:31 +0530

    Add files via upload

---
+++ Resource Files/Ghost.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    Renderer[] rends;
+    float alpha = 1;
+    float flashSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rends = this.gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rends)
+        {
+            r.material.SetFloat("_Mode", 3.0f);
+            r.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            r.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            r.material.SetInt("_ZWrite", 0);
+            r.material.DisableKeyword("_ALPHATEST_ON");
+            r.material.EnableKeyword("_ALPHABLEND_ON");
+            r.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            r.material.renderQueue = 3000;
+            r.material.color = new Color(1, 1, 1, alpha);
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (Renderer r in rends)
+        {
+            r.material.SetFloat("_Mode", 0.0f);
+            r.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            r.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            r.material.SetInt("_ZWrite", 1);
+            r.material.DisableKeyword("_ALPHATEST_ON");
+            r.material.DisableKeyword("_ALPHABLEND_ON");
+            r.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            r.material.renderQueue = -1;
+            r.material.color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha = 0.3f + Mathf.PingPong(Time.time * flashSpeed, 0.7f);
+        foreach (Renderer r in rends)
+        {
+            r.material.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
---
+++ Resource Files/SmoothFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 8.0f;
+    public float height = 1.5f;
+    public float heightOffset = 1.0f;
+    public float heightDamping = 4.0f;
+    public float rotationDamping = 2.0f;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target == null)
+            return;
+            
+        float wantedRotationAngle = target.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        transform.position = new Vector3(transform.position.x,
+                                currentHeight + heightOffset,
+                                transform.position.z);
+
+        transform.LookAt(target);
+    }
+}
